d48ef2c162c37711cab32aab67ec699488a24b4b
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-06 20:46:26 -0500

    finished?

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,24 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        String ans = "";
+        for(int i = 1; i <= 15; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                ans += "WuTang Forever";
+                //don't want another new line
+                break;
+            }
+            else if(i % 3 == 0) {
+                ans += "Wu";
+            }
+            else if(i % 5 == 0) {
+                ans += "Tang";
+            }
+            else {
+                ans += i; 
+            }
+            ans += "\n";
+        }
+        return ans;
     }
 }
