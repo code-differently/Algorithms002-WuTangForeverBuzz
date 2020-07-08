b555ab2ac7e37ea7fa40cd59323cdc6fbd0efc2e
Lucious Morrison <lmorrison1116@gmail.com>
2020-07-08 01:00:37 -0400

    completed

--- src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
@@ -10,8 +10,18 @@ public class WuTangForeverBuzzTest {
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
 
         String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTang Forever";
-        String actual = wuTangForeverBuzz.wuTangClan();
+        String actual = wuTangForeverBuzz.wuTangClan(15);
 
         Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void d3Test() {
+        WuTangForeverBuzz wu = new WuTangForeverBuzz();
+
+        boolean expected = false;
+        boolean actual = wu.divisibleBy3(0);
+
+        Assert.assertEquals(expected, actual);
     }
 }
