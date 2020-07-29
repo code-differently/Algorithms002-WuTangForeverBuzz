b9ad1f46a3cc036d0f316d8bd3f8976cef4c9d42
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-29 10:56:05 -0400

    fanetoooo

--- src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/hbcu/stay/ready/algorithms/WuTangForeverBuzzTest.java
@@ -8,9 +8,10 @@ public class WuTangForeverBuzzTest {
     @Test
     public void wuTangClanTest(){
         WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
+        int length = 15;
 
-        String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTang Forever";
-        String actual = wuTangForeverBuzz.wuTangClan();
+        String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTang Forever\n";
+        String actual = wuTangForeverBuzz.wuTangClan(length);
 
         Assert.assertEquals(expected,actual);
     }
